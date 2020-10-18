a3e0992448bd7faec39efec05fb9106750bd28bd
Ourcycle Sistemas <git@ourcycle.dev>
2020-10-17 23:01:32 -0300

    Remove favi.icon

--- server.js
+++ server.js
@@ -12,4 +12,4 @@ app.get('/*', function(req, res) {
   res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
 
-console.log('Console listening!');
+console.log('Console listening! ' + __dirname);
